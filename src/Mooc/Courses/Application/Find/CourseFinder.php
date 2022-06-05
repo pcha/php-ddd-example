b08ab49cc74c1a353eb4c37d4fbdd2ccf5e17d61
Pablo Cha <pablocha1992@gmail.com>
2022-06-05 12:44:37 +0200

    refactor(video) extract course finder as domain service

--- src/Mooc/Courses/Application/Find/CourseFinder.php
+++ src/Mooc/Courses/Application/Find/CourseFinder.php
@@ -5,24 +5,22 @@ declare(strict_types=1);
 namespace CodelyTv\Mooc\Courses\Application\Find;
 
 use CodelyTv\Mooc\Courses\Domain\Course;
+use CodelyTv\Mooc\Courses\Domain\Coursefinder as DomainCourseFinder;
 use CodelyTv\Mooc\Courses\Domain\CourseNotExist;
 use CodelyTv\Mooc\Courses\Domain\CourseRepository;
 use CodelyTv\Mooc\Shared\Domain\Courses\CourseId;
 
 final class CourseFinder
 {
-    public function __construct(private CourseRepository $repository)
+    private DomainCourseFinder $coursefinder;
+
+    public function __construct(CourseRepository $repository)
     {
+        $this->coursefinder = new DomainCourseFinder($this->repository);
     }
 
     public function __invoke(CourseId $id): Course
     {
-        $course = $this->repository->search($id);
-
-        if (null === $course) {
-            throw new CourseNotExist($id);
-        }
-
-        return $course;
+        return $this->coursefinder->__invoke($id);
     }
 }
